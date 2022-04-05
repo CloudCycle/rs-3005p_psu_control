7aa78b618de18637e5daa9a6b183cec8edf3a668
Ed Waugh <ed.waugh@cloudcycle.co>
2022-04-05 12:49:50 +0100

    Modified timings to suit faster measurement mode cycling

--- rs_3005p_control.py
+++ rs_3005p_control.py
@@ -2,22 +2,25 @@ import serial
 import datetime
 import time
 
-with serial.Serial(port='COM8', baudrate=9600, bytesize=8, parity='N', stopbits=1, timeout=1.0) as sdev:
+time_at_30v_s = (60*2) # Enough time to detect the higher voltage and get into measurement mode
+time_at_24v_s = (60*7) # Devices set to 5 minutes before entering idle after 24 V detected
+
+with serial.Serial(port='/dev/ttyACM0', baudrate=9600, bytesize=8, parity='N', stopbits=1, timeout=1.0) as sdev:
     sdev.write(b'*IDN?\n')
     print(sdev.readline().decode('utf-8').strip())
     sdev.write(b'VSET1:24.1\n')
     sdev.write(b'VSET1?\n')
     print('Set: {} volts'.format(sdev.readline().decode('utf-8').strip()))
-    sdev.write(b'ISET1:0.4\n')
+    sdev.write(b'ISET1:1.6\n')
     sdev.write(b'ISET1?\n')
     print('Set: {} Amps'.format(sdev.readline().decode('utf-8').strip()))
     sdev.write(b'OUT1\n')   # Turn on if not already
-    for i in range(1000):
+    for i in range(10000):
         sdev.write(b'VSET1:30.1\n')
         sdev.write(b'VSET1?\n')
-        print('{}, Count: {}, Set: {} volts'.format(datetime.datetime.now(), i, sdev.readline().decode('utf-8').strip()))
-        time.sleep(300)
+        print('{:%Y/%m/%d %H:%M:%S}, Count: {}, Set: {} volts for {:.1f} minutes'.format(datetime.datetime.now(), i, sdev.readline().decode('utf-8').strip(), time_at_30v_s/60.0))
+        time.sleep(time_at_30v_s)
         sdev.write(b'VSET1:24.1\n')
         sdev.write(b'VSET1?\n')
-        print('{}, Count: {}, Set: {} volts'.format(datetime.datetime.now(), i, sdev.readline().decode('utf-8').strip()))
-        time.sleep(120)
+        print('{:%Y/%m/%d %H:%M:%S}, Count: {}, Set: {} volts for {:.1f} minutes'.format(datetime.datetime.now(), i, sdev.readline().decode('utf-8').strip(), time_at_24v_s/60.0))
+        time.sleep(time_at_24v_s)
