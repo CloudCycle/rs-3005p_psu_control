42d94c6ce1772d774bc661b387ebbc2c4bed45fe
Ed Waugh <ed.waugh@cloudcycle.com>
2026-02-13 14:58:51 +0000

    add pylint and kbhit exit

--- rs_3005p_voltage_doubler_testing.py
+++ rs_3005p_voltage_doubler_testing.py
@@ -1,22 +1,23 @@
 '''Test the voltage doubler'''
-import datetime
 import time
+from msvcrt import kbhit
 import serial
 
-sequence = [{'time_s':1.0, 'voltage':12.0},
-            {'time_s':1.0, 'voltage':13.0},
-            {'time_s':1.0, 'voltage':14.0},
-            {'time_s':1.0, 'voltage':15.0},
-            {'time_s':1.0, 'voltage':16.0},
-            {'time_s':1.0, 'voltage':17.0},
-            {'time_s':1.0, 'voltage':18.0},
-            {'time_s':1.0, 'voltage':8.0},
-            {'time_s':1.0, 'voltage':9.0},
-            {'time_s':1.0, 'voltage':10.0},
-            {'time_s':1.0, 'voltage':11.0}
+sequence = [{'time_s':0.2, 'voltage':12.0},
+            {'time_s':0.2, 'voltage':13.0},
+            {'time_s':0.2, 'voltage':14.0},
+            {'time_s':0.2, 'voltage':15.0},
+            {'time_s':0.2, 'voltage':16.0},
+            {'time_s':0.2, 'voltage':0.0},
+            {'time_s':0.2, 'voltage':17.0},
+            {'time_s':0.2, 'voltage':18.0},
+            {'time_s':0.2, 'voltage':8.0},
+            {'time_s':0.2, 'voltage':9.0},
+            {'time_s':0.2, 'voltage':10.0},
+            {'time_s':0.2, 'voltage':10.2}
             ]
 
-with serial.Serial(port='COM11', baudrate=9600, bytesize=8, parity='N', stopbits=1, timeout=1.0) as sdev:
+with serial.Serial(port='COM11', baudrate=9600, bytesize=8, parity='N', stopbits=1, timeout=0.2) as sdev:
     sdev.write(b'*IDN?\n')
     print(sdev.readline().decode('utf-8').strip())
     sdev.write(b'VSET1:12.0\n')
@@ -34,3 +35,7 @@ with serial.Serial(port='COM11', baudrate=9600, bytesize=8, parity='N', stopbits
             set_voltage = sdev.readline().decode('utf-8').strip()
             print('Set: {} volts for {} seconds'.format(set_voltage, step['time_s']))
             time.sleep(step['time_s'])
+            if kbhit():
+                break
+        if kbhit():
+            break
