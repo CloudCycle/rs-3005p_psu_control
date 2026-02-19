5f1ee9e7e8ca8a5743e5d51061297564d62fc68f
Ed Waugh <ed.waugh@cloudcycle.com>
2026-02-19 08:24:49 +0000

    Added support for the TENMA 72 series PSU

--- tenma_72series_voltage_doubler.py
+++ tenma_72series_voltage_doubler.py
@@ -1,4 +1,6 @@
-'''Test the voltage doubler'''
+'''
+Tenma 72-13300 power supply, very similar to the RS branded one
+Test the voltage doubler'''
 import time
 from msvcrt import kbhit
 import serial
@@ -16,22 +18,22 @@ sequence = [{'time_s':1.5, 'voltage':12.0},
             {'time_s':1.5, 'voltage':11.0}
             ]
 
-with serial.Serial(port='COM11', baudrate=9600, bytesize=8, parity='N', stopbits=1, timeout=1.5) as sdev:
+with serial.Serial(port='COM13', baudrate=9600, bytesize=8, parity='N', stopbits=1, timeout=1.5) as sdev:
     sdev.write(b'*IDN?\n')
     print(sdev.readline().decode('utf-8').strip())
-    sdev.write(b'VSET1:11.5\n')
-    sdev.write(b'VSET1?\n')
+    sdev.write(b'VSET:11.5\n')
+    sdev.write(b'VSET?\n')
     set_voltage = sdev.readline().decode('utf-8').strip()
-    sdev.write(b'ISET1:5.0\n')
-    sdev.write(b'ISET1?\n')
+    sdev.write(b'ISET:20.0\n')
+    sdev.write(b'ISET?\n')
     print('Set: {} volts, {} Amps'.format(set_voltage, sdev.readline().decode('utf-8').strip()))
     sdev.write(b'OUT1\n')   # Turn on if not already
 
     for i in range(100):
         for step in sequence:
             try:
-                sdev.write('VSET1:{:.1f}\n'.format(step['voltage']).encode('utf-8'))
-                sdev.write(b'VSET1?\n')
+                sdev.write('VSET:{:.1f}\n'.format(step['voltage']).encode('utf-8'))
+                sdev.write(b'VSET?\n')
                 set_voltage = sdev.readline().decode('utf-8').strip()
                 print('Set: {} volts for {} seconds'.format(set_voltage, step['time_s']))
                 time.sleep(step['time_s'])
